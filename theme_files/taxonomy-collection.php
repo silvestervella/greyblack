b5f7597f3975f04de69b4d8d321214ec02556f05
Silvester Vella <silvestervella@Silvesters-iMac.lan>
2019-12-03 14:22:10 +0100

    jdfm

--- theme_files/taxonomy-collection.php
+++ theme_files/taxonomy-collection.php
@@ -1,35 +1,34 @@
-<?php get_header(); ?>
+<?php
+/*
+ * Template Name: Products Template
+ */
 
-	<main role="main">
-		<!-- section -->
-        <div class="prod-outer">
+ ?>
+ <?php get_header(); ?>
 
-        <div class="prod-left">
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-        </div>
-
-        <?php 
-		    $back_img = esc_url(get_post_meta(get_the_ID(), "post-back", true ));
-            
+ <main role="main">
+    <div class="posts-sec-outer">
+        <div class="container">            
+            <section id="products">
+            <?php
             $term = $wp_query->get_queried_object();
-            
-            $term_back_img = get_term_meta($term->term_id , "__term_meta_text");
-            $product_html = '';
-            $product_html .= '<div class="prod-right">';
-            $product_html .= '<div class="tax-back-img" style="background-image: url('.$term_back_img[0].')">';
-            $product_html .= '<div class="product-type-title">';
-            $product_html .=  $term->name;
-            $product_html .= '</div>';
-            $product_html .= '</div>'; // .prod-back-img
-            $product_html .= '</div>'; // .prod-right
 
-            $product_html .= '</div>'; // .prod-outer
-            echo $product_html;
-		?>
-	</main>
+                echo greyblack_generate_product_posts(array(
+                    'posts_per_page' => '0',
+                    'orderby' => 'date',
+                    'order' => 'ASC',
+                    'taxonomy' => $term->taxonomy,
+                    'terms' => $term->name ) );
 
-<?php get_sidebar(); ?>
+                wp_reset_postdata();
+            ?>
+            </section>
+        </div>
+        <!-- /container -->
+    </div>
+    <!-- /posts-sec-outer -->
+</main>
 
-<?php get_footer(); ?>
+<?php // get_sidebar(); ?>
+
+<?php get_footer();?>
